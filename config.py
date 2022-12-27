1c4380d902a2294e0e86d91bf2da52425b09a5dc
Kenny Deng <kennyddeng@gmail.com>
2022-12-27 06:29:50 -0500

    refactor: user configuration file

---
+++ config.py
@@ -0,0 +1,12 @@
+# interval time in seconds between samples
+sample_time = 30
+
+"""
+Modify as needed to suit the Artist/Album/Song you would like to get notified for and the sms message you would like to send
+"""
+
+url = "https://player.rogersradio.ca/chez/widget/now_playing"
+artist = "Billy Idol"
+# album = "Billy Idol"
+song = "White Wedding"
+message = "BILLY IDOL WHITE WEDDING - PLAYING NOW AT CHEZ106 - ITS A NICE DAY TO START AGAIN"
