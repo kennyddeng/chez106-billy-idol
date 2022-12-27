6c136bc16436a144f504a7655133e828db3cd158
Kenny Deng <kennyddeng@gmail.com>
2022-12-27 05:35:02 -0500

    feat: send sms using twilio

---
+++ keys.py
@@ -0,0 +1,5 @@
+account_sid = "YOUR_TWILIO_ACCOUNT_SID"
+auth_token = "YOUR_TWILIO_AUTH_TOKEN"
+
+twilio_number = "+YOUR_TWILIO_VIRTUAL_NUMBER"
+target_number = "+YOUR_TARGET_PHONE_NUMBER"
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import requests 
 import json
 import time
+import keys
+from twilio.rest import Client
 
+client = Client(keys.account_sid, keys.auth_token)
+message = client.messages.create(
+    body="BILLY IDOL WHITE WEDDING - PLAYING NOW AT CHEZ106 - ITS A NICE DAY TO START AGAIN",
+    from_=keys.twilio_number,
+    to=keys.target_number
+)
+
+# chez106 webpage player
 url = "https://player.rogersradio.ca/chez/widget/now_playing"
 data = requests.get(url).json()
 
+def sendSMS():
+    print(message.body)
+
 def lookForMusic():
     if (data["artist"] == "Billy Idol" and data["song_title"] == "White Wedding"):
-        print("send sms")
+        sendSMS()
 
 def loop():
     while True:
         lookForMusic()
         time.sleep(30)
-
 
 if __name__ == "__main__":
     loop()
