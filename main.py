95e3adc029a3f7fb86a9df252ed29ec999778658
Kenny Deng <kennyddeng@gmail.com>
2022-12-27 00:56:53 -0500

    feat: find when song white wedding is playing

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("testing")
+import requests 
+import json
+import time
+
+url = "https://player.rogersradio.ca/chez/widget/now_playing"
+data = requests.get(url).json()
+
+def lookForMusic():
+    if (data["artist"] == "Billy Idol" and data["song_title"] == "White Wedding"):
+        print("send sms")
+
+def loop():
+    while True:
+        lookForMusic()
+        time.sleep(30)
+
+
+if __name__ == "__main__":
+    loop()
